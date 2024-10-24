ecac15896fb0385b9f481f8914bc2c6d382e1055
Wojciech <s205838@sggw.edu.pl>
2024-10-24 21:37:47 +0200

    Add route, add signUp component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SingUpComponent } from './sing-up/sing-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'newAccount',
+    title: 'Create Eventer\'s account',
+    component: SingUpComponent
+}];
