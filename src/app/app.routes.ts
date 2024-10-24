4bf92e22dde4423c56212070283ad2322e03a62f
Wojciech <s205838@sggw.edu.pl>
2024-10-24 21:55:49 +0200

    Create login compoment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { SingUpComponent } from './sing-up/sing-up.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 export const routes: Routes = [{
-    path: 'newAccount',
+    path: 'singup',
     title: 'Create Eventer\'s account',
     component: SingUpComponent
+}, {
+    path: 'Login',
+    title: 'Log to Eventer\'s account',
+    component: LogInComponent
 }];
