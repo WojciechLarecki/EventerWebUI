21065f0b6376e53da5b5ace9b9fd75292e245159
Wojciech <s205838@sggw.edu.pl>
2024-10-25 23:59:02 +0200

    Add sign up view.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,24 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { SingUpComponent } from './sing-up/sing-up.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [{
-    path: 'singup',
-    title: 'Create Eventer\'s account',
-    component: SingUpComponent
-}, {
-    path: 'Login',
-    title: 'Log to Eventer\'s account',
-    component: LogInComponent
-}];
+export const routes: Routes = [
+    {
+        path: 'signup',
+        title: 'Create Eventer\'s account',
+        component: SingUpComponent
+    },
+    {
+        path: 'login',
+        title: 'Log to Eventer\'s account',
+        component: LogInComponent
+    },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
+
+// @NgModule({
+//     imports: [RouterModule.forRoot(routes)],
+//     exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
--- src/app/sing-up/sing-up.component.ts
+++ src/app/sing-up/sing-up.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sing-up',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sing-up.component.html',
   styleUrl: './sing-up.component.scss'
 })
