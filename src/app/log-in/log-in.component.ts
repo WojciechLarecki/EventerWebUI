af7e102586c5a4f3ca45acdbd2d7fdcfed8598c0
Wojciech <s205838@sggw.edu.pl>
2024-10-26 13:20:50 +0200

    Install template form in LogIn Comp.

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
 export class LogInComponent {
+  userEmail!: string;
+  userPassword!: string;
 
+  loginUser(): void {
+    alert(
+      `UserEmail: ${this.userEmail} | UserPassword: ${this.userPassword}`
+    );
+  }
 }
