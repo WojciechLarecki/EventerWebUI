4ec28dd1c963c20d793125ec3a834dc804926f6c
Wojciech <s205838@sggw.edu.pl>
2024-10-26 13:41:21 +0200

    Install template form in singup comp.

--- src/app/sing-up/sing-up.component.ts
+++ src/app/sing-up/sing-up.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LogoComponent } from '../logo/logo.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sing-up',
   standalone: true,
-  imports: [RouterLink, LogoComponent],
+  imports: [RouterLink, LogoComponent, FormsModule],
   templateUrl: './sing-up.component.html',
   styleUrl: './sing-up.component.scss'
 })
 export class SingUpComponent {
-
+  userPassword!: string;
+  userRepeatedPassword!: string;
+  userEmail!: string;
+  signupUser(): void {
+    alert(`UserEmail: ${this.userEmail} | 
+      UserPassword: ${this.userPassword} | 
+      UserRepeatedPassword: ${this.userRepeatedPassword}`);
+  }
 }
