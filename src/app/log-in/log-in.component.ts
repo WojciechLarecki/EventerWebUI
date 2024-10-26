236298b6679f8ab620c31d830eccd9469e9f2765
Wojciech <s205838@sggw.edu.pl>
2024-10-26 13:29:13 +0200

    Add logo component.

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms'
+import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, LogoComponent],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
--- src/app/sing-up/sing-up.component.ts
+++ src/app/sing-up/sing-up.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-sing-up',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, LogoComponent],
   templateUrl: './sing-up.component.html',
   styleUrl: './sing-up.component.scss'
 })
